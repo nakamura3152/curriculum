9a9a28dd2bcb0009e97458517e724415b2aebf9d
nakamura3152 <k-nakamura210239@cmind-co.jp>
2022-02-28 23:51:54 +0900

    Task2-2

--- JavaStudy/2-1/src/study/Main.java
+++ JavaStudy/2-1/src/study/Main.java
@@ -26,8 +26,7 @@ public class Main {
         //③ arr 配列をループさせ、isOdd関数を使って配列の要素が奇数かどうかの判定を行いなさい。
         //   要素が奇数の場合には「nは奇数です。」と出力されます。
         for(int n = 0; n < arr.length; n++) {
-        	  isOdd(arr[n]); 
-        	
+        	  isOdd(arr[n]);      	
         	}
     }
 

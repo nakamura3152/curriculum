ea679243bfbf55e5746af23413915fde3f014896
nakamura3152 <k-nakamura210239@cmind-co.jp>
2022-05-09 07:50:43 +0900

    Check2

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,35 @@
+package check;
+
+import constants.Constants;
+
+public class Check {
+	
+	private static String firstName= "中村";
+	private static String lastName= "健太";
+	
+	
+	public static void main(String[] args) {
+		
+	String fullName = plus(firstName,lastName);
+	
+	printName(fullName);
+	
+	
+	
+	Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+	pet.introduce();
+
+	RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+	robotPet.introduce();
+	}
+
+	private static String plus(String firstName, String lastName) {
+		return firstName + lastName;
+	}
+	
+	private static void printName(String fullName) {
+		System.out.println(fullName);
+	}
+	
+	
+}
